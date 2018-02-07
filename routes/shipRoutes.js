e2ac6af34ada2342c30bd3b2e4d489421ab95cd0
Andy <andy.dot.heng@gmail.com>
2018-02-07 18:55:16 -0500

    create POST route

--- routes/shipRoutes.js
+++ routes/shipRoutes.js
@@ -1,11 +1,11 @@
 // depdendencies
 const express = require("express");
 const app = express();
-// using routing
 const router = express.Router();
 // gives access to defined model and database methods (find, create, etc)
 const database = require("../models");
 
+// get all
 router.get("/", (req, res) => {
   database.Ship.find()
     .then(foundAll => {
@@ -14,6 +14,18 @@ router.get("/", (req, res) => {
     .catch(err => {
       res.send(err);
     })
-})
+});
+
+// create
+router.post("/", (req, res) => {
+  database.Ship.create(req.body)
+    .then(created => {
+      console.log(`${created.name} was added`);
+      res.json(created);
+    })
+    .catch(err => {
+      res.send(err);
+    })
+});
 
 module.exports = router;
